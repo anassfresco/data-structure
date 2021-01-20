c2a3f0dc8f5d5ae0199fb20920f386baf206c423
anaskirito <68207197+anassfresco@users.noreply.github.com>
2021-01-20 21:42:20 +0100

    Update binary_tree2.py

--- binary_tree2.py
+++ binary_tree2.py
@@ -24,6 +24,7 @@ class binary_tree:
                 self.insert_recusion(data, node.right_node)
             else:
                 node.right_node = new_node
+
     def maxNode(self):
         if self.tree is None:
             return
@@ -70,114 +71,117 @@ class binary_tree:
             self.traverse_prerecusion(node.left_node)
         elif  node.right_node:
             self.traverse_prerecusion(node.right_node)
-    # def removeNode(self,data,node):
-    #     if not node:
-    #         return node
-    #     if data<node.data:
-    #         node.left_node=self.removeNode(data,node.left_node)
-    #     elif data>node.data:
-    #         node.right_node=self.removeNode(data,node.right_node)
-    #     else:
-    #         if not node.right_node and not node.left_node:
-    #             del node
-    #             return None
-    #         if not node.left_node:
-    #             node1=node.right_node
-    #             del node
-    #             return node1
-    #         elif not node.right_node:
-    #             node2=node.left_node
-    #             del node
-    #             return node2
-    #         node_with_two_node=self.predecessor(node.left_node)
-    #         node.data=node_with_two_node.data
-    #         node.left_node=self.removeNode(node_with_two_node.data,node.left_node)
-    #     return node
-    # def predecessor(self,node):
-    #     if node.right_node:
-    #         return self.predecessor(node.right_node)
-    #     return node
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    # def remove(self,data):
-    #     if self.tree:
-    #         self.tree=self.removeNode(data,self.tree)
-
-
-    def remove_item(self, data):
-        if self.tree is None:
-            return False
-        if self.tree.data == data:
-            if not self.tree.right_node and not self.tree.left_node:
-                self.tree = None
-            elif self.tree.left_node and self.tree.right_node is None:
-                self.tree = self.tree.left_node
-            elif self.tree.left_node and self.tree.right_node:
-                self.remove_node(self.tree)
-
-        # find root to remove
-        parent = None
-        node = self.tree
-        while node and node.data != data:
-            parent = node
-            if data < node.data:
-                node = node.left_node
-            elif data > node.data:
-                node = node.right_node
-        # case data not found
-        if node is None or node.data != data:
-            return False
-        # case data has no children
-        elif node.right_node is None and node.left_node is None:
-            if data > parent.data:
-                parent.right_node = None
-            else:
-                parent.left_node = None
-            return True
-        # case data has left child
-        elif node.right_node is None and node.left_node is not None:
-            if data > parent.data:
-                parent.right_node = node.left_node
-            else:
-                parent.left_node = node.left_node
-            return True
-        # case data has right child only
-        elif node.right_node is not None and node.left_node is None:
-            if data > parent.data:
-                parent.right_node = node.left_node
-            else:
-                parent.left_node = node.left_node
-            return True
-        elif node.right_node and node.left_node:
-            self.remove_node(node)
-
-    def remove_node(self, node):
-        delparent = node
-        node_delate = node.right_node
-        while node_delate.left_node:
-            delparent = node_delate
-            node_delate = node_delate.left_node
-        node.data = node_delate.data
-        if node_delate.right_node:
-            if delparent.data > node_delate.data:
-                delparent.left_node = node_delate.right_node
-            else:
-                delparent.right_node = node_delate.right_node
+    def removeNode(self,data,node):
+        if not node:
+            return node
+        if data<node.data:
+            node.left_node=self.removeNode(data,node.left_node)
+        elif data>node.data:
+            node.right_node=self.removeNode(data,node.right_node)
         else:
-            if delparent.data > node_delate.data:
-                delparent.left_node = None
-            else:
-                delparent.right_node = None
+            if not node.right_node and not node.left_node:
+                del node
+                return None
+            if not node.left_node:
+                node1=node.right_node
+                del node
+                return node1
+            elif not node.right_node:
+                node2=node.left_node
+                del node
+                return node2
+            node_with_two_node=self.predecessor(node.left_node)
+            node.data=node_with_two_node.data
+            node.left_node=self.removeNode(node_with_two_node.data,node.left_node)
+        return node
+    def predecessor(self,node):
+        if node.right_node:
+            return self.predecessor(node.right_node)
+        return node
+
+
+
+
+
+
+
+
+
+
+
+
+    def remove(self,data):
+        if self.tree:
+            self.tree=self.removeNode(data,self.tree)
+
+
+    # def remove_item(self, data):
+    #     if self.tree is None:
+    #         return False
+    #     if self.tree.data == data:
+    #         if not self.tree.right_node and not self.tree.left_node:
+    #             self.tree = None
+    #         elif self.tree.left_node and self.tree.right_node is None:
+    #             self.tree = self.tree.left_node
+    #         elif self.tree.left_node and self.tree.right_node:
+    #             self.remove_node(self.tree)
+    # 
+    #     # find root to remove
+    #     parent = None
+    #     node = self.tree
+    #     while node and node.data != data:
+    #         parent = node
+    #         if data < node.data:
+    #             node = node.left_node
+    #         elif data > node.data:
+    #             node = node.right_node
+    #     # case data not found
+    #     if node is None or node.data != data:
+    #         return False
+    #     # case data has no children
+    #     elif node.right_node is None and node.left_node is None:
+    #         if data > parent.data:
+    #             parent.right_node = None
+    #         else:
+    #             parent.left_node = None
+    #         return True
+    #     # case data has left child
+    #     elif node.right_node is None and node.left_node is not None:
+    #         if data > parent.data:
+    #             parent.right_node = node.left_node
+    #         else:
+    #             parent.left_node = node.left_node
+    #         return True
+    #     # case data has right child only
+    #     elif node.right_node is not None and node.left_node is None:
+    #         if data > parent.data:
+    #             parent.right_node = node.left_node
+    #         else:
+    #             parent.left_node = node.left_node
+    #         return True
+    #     elif node.right_node and node.left_node:
+    #         self.remove_node(node)
+    # 
+    # def remove_node(self, node):
+    #     delparent = node
+    #     node_delate = node.right_node
+    #     while node_delate.left_node:
+    #         delparent = node_delate
+    #         node_delate = node_delate.left_node
+    #     node.data = node_delate.data
+    #     if node_delate.right_node:
+    #         if delparent.data > node_delate.data:
+    #             delparent.left_node = node_delate.right_node
+    #         else:
+    #             delparent.right_node = node_delate.right_node
+    #     else:
+    #         if delparent.data > node_delate.data:
+    #             delparent.left_node = None
+    #         else:
+    #             delparent.right_node = None
+    # def show(self):
+    #     print(self.tree.data)
+
 
 
 binary=binary_tree()
@@ -187,6 +191,7 @@ binary.insert(44)
 binary.insert(64)
 binary.insert(94)
 binary.insert(32)
-binary.remove_item(76)
+binary.remove(44)
+
 
 print(binary.traverse())
