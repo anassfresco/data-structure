475a6a0c671a5781b387eb85c57160f5700cd196
anaskirito <68207197+anassfresco@users.noreply.github.com>
2021-01-24 22:09:05 +0100

    Update heap.py

--- heap.py
+++ heap.py
@@ -2,13 +2,13 @@ class Heap(object):
     HEAP_SIZE = 10
 
     def __init__(self):
-        self.heap = [0] * Heap.HEAP_SIZE;
-        self.currentPosition = -1;
+        self.heap = [0] * Heap.HEAP_SIZE
+        self.currentPosition = -1
 
     def insert(self, item):
 
         if self.isFull():
-            print("Heap is full..");
+            print("Heap is full..")
             return
 
         self.currentPosition = self.currentPosition + 1
@@ -22,7 +22,7 @@ class Heap(object):
         while parentIndex >= 0 and self.heap[parentIndex] < self.heap[index]:
             temp = self.heap[index]
             self.heap[index] = self.heap[parentIndex]
-            self.heap[parentIndex] = temp;
+            self.heap[parentIndex] = temp
             parentIndex = (int)((index - 1) / 2)
 
     def heapsort(self):
@@ -36,26 +36,32 @@ class Heap(object):
 
     def fixDown(self, index, upto):
 
-        b = 0
-        while b == 0:
-            b = 1
-            i = index
-            while True:
-                left_child = int(2 * i + 1)
-                right_child = int(2 * i + 2)
-                if left_child > upto:
-                    break
-                elif self.heap[i] < self.heap[left_child]:
-                    self.heap[i], self.heap[left_child] = self.heap[left_child], self.heap[i]
-                    b = 0
-                elif right_child > 5:
-                    break
-                elif self.heap[i] < self.heap[right_child]:
-                    self.heap[i], self.heap[right_child] = self.heap[right_child], self.heap[i]
-                    b = 0
+        while index <= upto:
+
+            leftChild = 2 * index + 1
+            rightChild = 2 * index + 2
+
+            if leftChild < upto:
+                childToSwap = None
+
+                if rightChild > upto:
+                    childToSwap = leftChild
                 else:
-                    pass
-                i = i + 1
+                    if self.heap[leftChild] > self.heap[rightChild]:
+                        childToSwap = leftChild
+                    else:
+                        childToSwap = rightChild
+
+                if self.heap[index] < self.heap[childToSwap]:
+                    temp = self.heap[index]
+                    self.heap[index] = self.heap[childToSwap]
+                    self.heap[childToSwap] = temp
+                else:
+                    break
+
+                index = childToSwap
+            else:
+                break;
 
     def isFull(self):
         if self.currentPosition == Heap.HEAP_SIZE:
@@ -72,6 +78,11 @@ if __name__ == "__main__":
     heap.insert(2)
 
     heap.heapsort()
+
+
+
+
+
 
 
 
